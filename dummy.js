ae27fc73588876b82c382242b174149c2f29bea8
ac-vishnumali <160574479+ac-vishnumali@users.noreply.github.com>
2024-03-27 19:43:38 +0530

    Update dummy.js

--- dummy.js
+++ dummy.js
@@ -1,29 +1,29 @@
 // This file contains dummy information data
 var token = vishnu_ppZ67
-var SECRET_TOKEN=example_token123
-var API_KEY=example_api_key456
-var PASSWORD=example_password789PROD\MSSQL
+var SECRET_TOKEN="example_token123"
+var API_KEY="example_api_key456"
+var PASSWORD="example_password789PROD\MSSQL"
 var header =  "Authorization: Basic QWxhZGRpbjpPcGVuU2VzYW1l"
 
 # Custom API keys or tokens
-CUSTOM_API_KEY=your_custom_api_key_here
-CUSTOM_API_TOKEN=your_custom_api_token_here
+CUSTOM_API_KEY=`your_custom_api_key_here`
+CUSTOM_API_TOKEN=`your_custom_api_token_here`
 
 # Internal URLs or endpoints
-INTERNAL_API_URL=http://internal-api.example.com
-INTERNAL_DB_ENDPOINT=mysql://internal-db.example.com:3306/database_name
+INTERNAL_API_URL=`http://internal-api.example.com`
+INTERNAL_DB_ENDPOINT=`mysql://162.23.45.213:3306/sys`
 
 # Configuration settings
-ENCRYPTION_KEY=your_encryption_key_here
-DB_CONNECTION_STRING=your_database_connection_string_here
+ENCRYPTION_KEY=`your_encryption_key_here`
+DB_CONNECTION_STRING=`your_database_connection_string_here`
 
 # Custom secrets or credentials
-CUSTOM_SECRET=my_custom_secret_here
-CUSTOM_CREDENTIALS=username:password
+CUSTOM_SECRET=`my_custom_secret_here`
+CUSTOM_CREDENTIALS=`username:password`
 
 # License keys or activation codes
-LICENSE_KEY=your_license_key_here
-ACTIVATION_CODE=your_activation_code_here
+LICENSE_KEY=`your_license_key_here`
+ACTIVATION_CODE=`your_activation_code_here`
 
 const rsaPrivateKey = `-----BEGIN RSA PRIVATE KEY-----
 MIIEpAIBAAKCAQEA4mzPoj4XlO3oxKoanJrKCyGgJ4m98MCtlA8l9IrSo+AKtHdp
@@ -36,27 +36,27 @@ c2GNKp/jCvMfJfnt8PtDut2TUKsjCT2+vp0w0TcZx/ZvNz+J9mfUA4d6VytT8U6z
 
 var key1="-----BEGIN OPENSSH PRIVATE KEY-----cTrx9VGoMgJGUjChtcSkfWfUbHw/+t2Xz4b1uF03sUwZfYn7Xs4yoCQ9vSnPJbTJTtbjAwAAN2DiUljUcZ3vm3lnMDJ1XX72sn/Bw+g1u9kiV7Rg/qSuhPVbLYrlNnbTNWEepi9xigxSSlC2VZLZkH+O7DZL3CQLB5e2j9pG8tMjDJfl9bMw8IbCykJuuyuUBbUmMprNI8rL56QUaQwNJqzbE9G6WzCpzwwD6Cm9aBo7hHXwNQlQjQrKvqg53dmzQ7HQCV3moJffEzV7aP6TWZvjok1y6CX4a2r6+5zny6Pt11oAwR55ZL8xMsnebIYv0zWp0IznpJR6VYQfumquWl1gIlnx+ubexre1zqlyftgCtWqJeO88szS7bAq3txhs3aJ2dXZJWlI1fBEnXOV6jQdAf7tNm7jtIyOeLZouTxDzWrM2g4xnuR3SgTc3eZ7CWFCWio4XfR5YjS1F/kkgp5e8W8Zl5w4aJ07KFavZu/WuGxT1foj6Sk+6IFRZdyG5VtPczBeo9Zle4Y5P5RlnDx8vD3UKd0E6MfbwA+iBEGlCgDsjHgHd46irFhGtgriZDb05zQjyVUkJRYKx1tSKx+rgV+4wT3xLfZmQd4fIc7/eg==-----END OPENSSH PRIVATE KEY-----"
 
-var dbUrl= "mongodb://myusername:mypassword@cluster0.mongodb.net:27017/mydatabase"
+var dbUrl= "mongodb://abhishek:378huygy7GTF@cluster0.mongodb.net:27017/mydatabase"
 
 # Custom API keys or tokens
 MY_CUSTOM_API_KEY=abc123xyz456
 SPECIAL_API_TOKEN=secret_token_789
 
 # Internal URLs or endpoints
-DEV_INTERNAL_API=http://dev.internal-api.example.com
-PROD_DB_ENDPOINT=postgres://prod-db.example.com:5432/mydatabase
+DEV_INTERNAL_API=`http://dev.internal-api.example.com`
+PROD_DB_ENDPOINT=`postgres://prod-db.example.com:5432/mydatabase`
 
 # Configuration settings
-SECRET_ENCRYPTION_KEY=my_secret_encryption_key
-CUSTOM_DB_CONNECTION_STRING=jdbc:mysql://custom-db.example.com:3306/mydatabase
+SECRET_ENCRYPTION_KEY=`my_secret_encryption_key`
+CUSTOM_DB_CONNECTION_STRING=`jdbc:mysql://custom-db.example.com:3306/mydatabase`
 
 # Custom secrets or credentials
 SUPER_SECRET_KEY=super_secret_key_value
 USER_CREDENTIALS=admin:admin@123
 
 # License keys or activation codes
-DEMO_LICENSE_KEY=DEMO-123-456-789
-TEST_ACTIVATION_CODE=TEST-ABC-DEF-123
+DEMO_LICENSE_KEY=`DEMO-123-456-789`
+TEST_ACTIVATION_CODE=`TEST-ABC-DEF-123`
 
 
 var dummy_info = {
